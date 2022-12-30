5f7438a6e644f16a259ba9fddce7aa26fbaf9bfb
Igor Sukharev <imixaly4@gmail.com>
2022-12-30 19:33:15 +0200

    add favicons

---
+++ README.md
@@ -0,0 +1,10 @@
+# Tracker
+
+Time/Money Tracker Application
+
+[Application Website](https://isumix.github.io/tracker/)
+
+## Features
+
+- Installs to home screen, behaves as native application (PWA)
+- Works offline
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-!function(){"use strict";self.addEventListener("fetch",(function(e){}));[{'revision':'24865decf08bf1b9fa298e479bb75b2a','url':'/tracker/index.html'},{'revision':null,'url':'/tracker/static/js/main.bd9d4854.js'}]}();
+!function(){"use strict";self.addEventListener("fetch",(function(e){}));[{'revision':'e59b22134b0cd178c0bf7618f17bea52','url':'/tracker/index.html'},{'revision':null,'url':'/tracker/static/js/main.bd9d4854.js'}]}();
