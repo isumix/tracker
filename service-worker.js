5ac78c7a9182c80464d158b921399118280a2aef
Igor Sukharev <imixaly4@gmail.com>
2023-01-02 22:06:08 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-!function(){"use strict";self.addEventListener("fetch",(function(e){}));[{'revision':'1657928cc4815c56b5fa5bfde593923b','url':'/tracker/index.html'},{'revision':null,'url':'/tracker/static/js/main.36e2d080.js'}]}();
+!function(){"use strict";self.addEventListener("fetch",(function(e){}));[{'revision':'0c84a11f2edb850741061cba638787ae','url':'/tracker/index.html'},{'revision':null,'url':'/tracker/static/js/main.93c4dd58.js'}]}();
